2e7daf9d4d599e3daac819652bd73377520bbe0c
ararararai <usd1122334@gmail.com>
2026-04-13 22:23:18 +0900

    Update layout: remove NO keys, add Space width

--- keyboards/ararararai/gamesir_vx2/keymaps/vial/keymap.c
+++ keyboards/ararararai/gamesir_vx2/keymaps/vial/keymap.c
@@ -2,25 +2,25 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        // ROW0: ESC, NO,   F1,   F2,   F3,   F4,   F5
-        KC_ESC,  KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-        // ROW1: `,  1,    2,    3,    4,    5,    6
+        // ROW0: ESC, F1, F2, F3, F4, F5
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+        // ROW1: `, 1, 2, 3, 4, 5, 6
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
-        // ROW2: TAB, Q,   W,    E,    R,    NO,   T
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_NO,   KC_T,
-        // ROW3: CAPS, A,  S,    D,    F,    NO,   G
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_NO,   KC_G,
-        // ROW4: LSFT, Z,  X,    C,    V,    NO,   B
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,   KC_B,
-        // ROW5: LCTL, MO1, LALT, SPC, NO,   NO,   M
-        KC_LCTL, MO(1),   KC_LALT, KC_SPC,  KC_NO,   KC_NO,   KC_M
+        // ROW2: TAB, Q, W, E, R, T
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        // ROW3: CAPS, A, S, D, F, G
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        // ROW4: LSFT, Z, X, C, V, B
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+        // ROW5: LCTL, Fn, LALT, SPC, M
+        KC_LCTL, MO(1),   KC_LALT, KC_SPC,  KC_M
     ),
     [1] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 };
