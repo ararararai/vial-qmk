49ef1142fa95dbf1aa5805151fb0f736c5f2e260
ararararai <usd1122334@gmail.com>
2026-04-15 12:01:39 +0900

    add 2_3_testkey

---
+++ keyboards/ararararai/2_3_testkey/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+#define VIAL_KEYBOARD_UID {0x6F, 0x6F, 0x8A, 0xBE, 0xEE, 0x3F, 0x04, 0x1B}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
---
+++ keyboards/ararararai/2_3_testkey/readme.md
@@ -0,0 +1,8 @@
+# 2_3_testkey
+
+2x3 test macropad for Elecrow ordering practice.
+
+- 6 keys (2 rows x 3 cols)
+- Kailh Choc V2 hotswap
+- Raspberry Pi Pico (RP2040)
+- Vial-QMK firmware
